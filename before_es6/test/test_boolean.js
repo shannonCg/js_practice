cafe84f759dfae6c1befc2aa309a4005bd8b3c11
shannonCg <phy8233@gmail.com>
2021-02-18 23:13:05 +0800

    refactor

---
+++ before_es6/test/test_boolean.js
@@ -0,0 +1,14 @@
+if('') //false
+print('empty string');
+if(undefined) //false
+print('undefined');
+if(null) //false
+print('null');
+if(NaN) //false
+print('NaN');
+if(0) //false
+print('0');
+if([]) //true
+print('[]');
+if({}) //true
+print('{}');
